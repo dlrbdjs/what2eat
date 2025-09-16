190d29a8da6da3c2ec720179c74bba006a8f6888
Gyueon Lee <gon2yq@naver.com>
2025-09-16 22:38:45 +0900

    Init Project

---
+++ src/test/java/site/what2eat/meal/MealApplicationTests.java
@@ -0,0 +1,13 @@
+package site.what2eat.meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
