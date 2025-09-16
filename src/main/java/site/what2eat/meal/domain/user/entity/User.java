11d5dfb4f41655fd578efdf86529d4ad73b01ca5
Gyueon Lee <gon2yq@naver.com>
2025-09-16 23:26:05 +0900

    ✨feat: User 엔티티 정의

---
+++ src/main/java/site/what2eat/meal/domain/user/entity/User.java
@@ -0,0 +1,23 @@
+package site.what2eat.meal.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "member")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "isSent", nullable = false)
+    private boolean isSent;
+}
